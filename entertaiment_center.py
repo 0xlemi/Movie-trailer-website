a28284f23891f7a6f72d8e96a2b0eec072ee001e
lem93 <lem.espinosa@yahoo.com>
2015-08-16 14:36:30 -0600

    adding tvShows and making them sons of videos

--- entertaiment_center.py
+++ entertaiment_center.py
@@ -1,38 +1,79 @@
 import media
 import fresh_tomatoes
 
+#***********
+#MOVIES
+#***********
 movie_title = "Toy Story"
 movie_storyline = "A Story of toys that come to life."
 movie_image_url = "http://www.rotoscopers.com/wp-content/uploads/2013/10/Toy-Story-Poster.jpg"
 movie_youtube_url = "https://youtu.be/0q1rxc96m2w"
-toy_story = media.Movie(movie_title, movie_storyline, movie_image_url, movie_youtube_url);
-
+date_on_theater = "19-nov-1995"
+duration = "81"
+toy_story = media.Movie(movie_title, duration, movie_storyline, movie_image_url, movie_youtube_url, date_on_theater)
 movie_title = "Avatar"
 movie_storyline = "Army on alien planet."
 movie_image_url = "http://orig15.deviantart.net/a0e7/f/2010/192/b/e/avatar_special_edition_poster_by_j_k_k_s.jpg"
 movie_youtube_url = "https://youtu.be/5PSNL1qE6VY"
-avatar = media.Movie(movie_title, movie_storyline, movie_image_url, movie_youtube_url);
-
+date_on_theater = "18-dic-2009"
+duration = "162"
+avatar = media.Movie(movie_title, duration, movie_storyline, movie_image_url, movie_youtube_url, date_on_theater)
 movie_title = "School of rock"
 movie_storyline = "A guy that teaches kids rock."
 movie_image_url = "http://static.rogerebert.com/uploads/movie/movie_poster/school-of-rock-2003/large_cREN222Yw78zvSQ9bg17Y9QZS0c.jpg"
 movie_youtube_url = "https://youtu.be/XCwy6lW5Ixc"
-school_of_rock = media.Movie(movie_title, movie_storyline, movie_image_url, movie_youtube_url);
-
+date_on_theater = "3-oct-2003"
+duration = "108"
+school_of_rock = media.Movie(movie_title, duration, movie_storyline, movie_image_url, movie_youtube_url, date_on_theater)
 movie_title = "The Dark Knight"
 movie_storyline = "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice."
 movie_image_url = "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SY317_CR0,0,214,317_AL_.jpg"
 movie_youtube_url = "https://youtu.be/EXeTwQWrcwY"
-the_dark_knight = media.Movie(movie_title, movie_storyline, movie_image_url, movie_youtube_url);
-
+date_on_theater = "18-jul-2008"
+duration = "152"
+the_dark_knight = media.Movie(movie_title, duration, movie_storyline, movie_image_url, movie_youtube_url, date_on_theater)
 movie_title = "Friends with Benefits"
 movie_storyline = "While trying to avoid the cliches of Hollywood romantic comedies, Dylan Harper and Jamie Rellis soon discover however that adding the act of sex to their friendship does lead to complications."
 movie_image_url = "http://ia.media-imdb.com/images/M/MV5BMTQ2MzQ0NTk4N15BMl5BanBnXkFtZTcwMDc2NDYzNQ@@._V1_SY317_CR0,0,214,317_AL_.jpg"
 movie_youtube_url = "https://youtu.be/iJS-wWqVAyk"
-friends_with_benefits = media.Movie(movie_title, movie_storyline, movie_image_url, movie_youtube_url);
+date_on_theater = "22-jul-2011"
+duration = "109"
+friends_with_benefits = media.Movie(movie_title, duration, movie_storyline, movie_image_url, movie_youtube_url, date_on_theater)
+#***********
+#TVSHOWS
+#***********
+title = "Breaking Bad"
+storyline = "A chemistry teacher diagnosed with a terminal lung cancer teams up with his former student to cook and sell crystal meth."
+image_url = "http://ia.media-imdb.com/images/M/MV5BMTQ0ODYzODc0OV5BMl5BanBnXkFtZTgwMDk3OTcyMDE@._V1_SY317_CR0,0,214,317_AL_.jpg"
+youtube_url = "https://youtu.be/HhesaQXLuRY"
+duration = "41"
+num_seasons = "5"
+breaking_bad = media.TvShow(title, duration, storyline, image_url, youtube_url, num_seasons)
 
-#print(avatar.storyline)
-#avatar.show_trailer()
+title = "Game of Thrones"
+storyline = "Several noble families fight for control of the mythical land of Westeros."
+image_url = "http://ia.media-imdb.com/images/M/MV5BNTgxOTI4NzY2M15BMl5BanBnXkFtZTgwMjY3MTM2NDE@._V1_SX214_AL_.jpg"
+youtube_url = "https://youtu.be/8ixEWrTLiZg"
+duration = "55"
+num_seasons = "6"
+game_of_thrones = media.TvShow(title, duration, storyline, image_url, youtube_url, num_seasons)
 
-movies = [toy_story, avatar, school_of_rock, the_dark_knight, friends_with_benefits]
-fresh_tomatoes.open_movies_page(movies)
+title = "Cosmos"
+storyline = "Astronomer Carl Sagan leads us on an engaging guided tour of the various elements and cosmological theories of the universe."
+image_url = "http://ia.media-imdb.com/images/M/MV5BMTI0NTg5MjA5OF5BMl5BanBnXkFtZTcwNDMxMTEyMQ@@._V1_SY317_CR13,0,214,317_AL_.jpg"
+youtube_url = "https://youtu.be/bSxHZPoQ4JQ"
+duration = "60"
+num_seasons = "1"
+cosmos = media.TvShow(title, duration, storyline, image_url, youtube_url, num_seasons)
+
+
+
+
+# print(cosmos.storyline)
+# print(cosmos.num_seasons)
+# print(cosmos.duration)
+# print(cosmos.title)
+# cosmos.show_trailer()
+
+movies_tvshow = [toy_story, avatar, school_of_rock, the_dark_knight, friends_with_benefits, breaking_bad, game_of_thrones, cosmos]
+fresh_tomatoes.open_movies_page(movies_tvshow)
--- media.py
+++ media.py
@@ -1,13 +1,29 @@
 import webbrowser
 
-class Movie():
-	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-	def __init__(self, movie_title, movie_storyline, movie_image_url, movie_youtube_url):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = movie_image_url
-		self.trailer_youtube_url = movie_youtube_url
+class Video():
+
+	def __init__(self, title, duration_minutes, storyline, poster_url , trailer_url):
+		self.title = title
+		self.duration = duration_minutes
+		self.storyline = storyline
+		self.poster_image_url = poster_url
+		self.trailer_youtube_url = trailer_url
 
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
+
+
+
+class Movie(Video):
+	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+	def __init__(self, title, duration_minutes, storyline, poster_url , trailer_url, date_on_theater):
+		Video.__init__(self, title, duration_minutes, storyline, poster_url , trailer_url)
+		self.date_on_theater = date_on_theater
+
+class TvShow(Video):
+	def __init__(self, title, duration_minutes, storyline, poster_url , trailer_url, num_seasons):
+		Video.__init__(self, title, duration_minutes, storyline, poster_url , trailer_url)
+		self.num_seasons = num_seasons
+
